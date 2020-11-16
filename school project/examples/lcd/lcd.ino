7ef1bb5b70f7b1b135eca591c1d4d7ff9b0c327f
marioJfa <mariojeries@gmail.com>
2020-11-16 12:12:36 +0200

    Create lcd.ino

---
+++ school project/examples/lcd/lcd.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+const int rs = 7, en = 6, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
