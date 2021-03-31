0024150cc7ae5f4aa9c252c91755b7b364777741
marioJfa <mariojeries@gmail.com>
2021-03-31 17:25:49 +0300

    .

--- school project/documents/Final/Final.ino
+++ school project/documents/Final/Final.ino
@@ -1,8 +1,8 @@
 //LCD
 //identifying LCD related variables
-int LCD_Address = 0x27;
-int LCD_Columns = 16;
-int LCD_Rows = 2;
+#define LCD_Address 0x3F
+#define LCD_Columns 16
+#define LCD_Rows 2
 int LCD_status_updated = 0;
 //end
 
@@ -12,12 +12,12 @@ int LCD_status_updated = 0;
 int Motor_R_Speed  = 5, Motor_R_Dir = 6, Motor_L_Speed =9, Motor_L_Dir = 10;
 int turn[2]; //[turn_value, turn_Dir]
 int turning_forward_direction = 0,turning_reverse_direction = 1;
-int Max_turn_value = 180;
+int Max_turn_value = 50;
 int Min_turn_value = 10;
 int throttle[2]; //[throttle_value, throttle_Dir]
 int throttle_forward_direction = 0,throttle_reverse_direction = 1;
 int Max_throttle_value = 255;
-int Min_throttle_value = 25; 
+int Min_throttle_value = 100; 
 int v1, v2;
 
 // end
@@ -25,46 +25,34 @@ int v1, v2;
 
 //including essintial libraries
 
-  #include <BlynkSimpleSerialBLE.h>
   //For LCD
   #include <Wire.h> 
   #include <LiquidCrystal_I2C.h>
+  #include <SoftwareSerial.h>
 
 //end
 
 
 //identifying essential connection variables
 
-#define BLYNK_USE_DIRECT_CONNECT
-char auth[] = "???";
+SoftwareSerial Bluetooth(2,3);
 
 //end
 
 
-BLYNK_WRITE(V1) //turn
-{
-   v1 = (param.asInt()); // assigning incoming value from pin V1 to a variable
-}
-
-BLYNK_WRITE(V2) //throttle
-{
-   v2 = (param.asInt()); // assigning incoming value from pin V2 to a variable
-}
-
-
 //turning functions
 
-int get_turn_val(int turn_input_val = v1){
-  if(turn_input_val<500){
-    turn[0] = map(turn_input_val,500,0,Min_turn_value,Max_turn_value);
+int get_turn_val(int turn_input_val = turn[0]){
+  if((turn_input_val%2)==0){
+    turn[0] = map(turn_input_val,0,255,Min_turn_value,Max_turn_value);
     turn[1] = turning_forward_direction;
   }
-  else if(524<turn_input_val && turn_input_val<=1024){
-    turn[0] = map(turn_input_val,524,1024,Min_turn_value,Max_turn_value);
+  else if((turn_input_val%2)==1){
+    turn[0] = map(turn_input_val,0,255,Min_turn_value,Max_turn_value);
     turn[1] = turning_reverse_direction;
   }
   else{
-    turn[0] =0;
+    turn[0] =2;
   }
   return turn;
 }
@@ -74,13 +62,13 @@ int get_turn_val(int turn_input_val = v1){
 
 //throttling functions
 
-int get_throttle_value(int throttle_input_val = v2){
-  if(throttle_input_val<500){
-    throttle[0] = map(throttle_input_val,500,0,Min_throttle_value,Max_throttle_value);
+int get_throttle_value(int throttle_input_val = throttle[0]){
+  if((throttle_input_val%2)==1){
+    throttle[0] = map(throttle_input_val,0,255,Min_throttle_value,Max_throttle_value);
     throttle[1] = throttle_forward_direction;
   }
-  else if(524<throttle_input_val && throttle_input_val<=1024){
-    throttle[0] = map(throttle_input_val,524,1024,Min_throttle_value,Max_throttle_value);
+  else if((throttle_input_val%2)==0){
+    throttle[0] = map(throttle_input_val,0,255,Min_throttle_value,Max_throttle_value);
     throttle[1] = throttle_reverse_direction;
   }
   else{
@@ -108,6 +96,12 @@ void Turn_Left(){
   analogWrite(Motor_L_Speed, throttle[0]-turn[0]);
   digitalWrite(Motor_L_Dir, throttle[1]);
 }
+void Linear(){
+  analogWrite(Motor_R_Speed, throttle[0]);
+  digitalWrite(Motor_R_Dir, throttle[1]);
+  analogWrite(Motor_L_Speed, throttle[0]);
+  digitalWrite(Motor_L_Dir, throttle[1]);
+}
 //end
 
 void drive(){
@@ -122,6 +116,9 @@ void drive(){
   else if(turn[1] == 0){
     Turn_Left();
   }
+  else if(turn[1] ==2){
+    Linear();
+  }
 }
 
 //end
@@ -131,38 +128,40 @@ LiquidCrystal_I2C lcd(LCD_Address,LCD_Columns,LCD_Rows);
 //end
 
 void LCD(){
-  if(LCD_status_updated == 0){
+//  if(LCD_status_updated == 0){
    lcd.setCursor(1,1);
    lcd.print("Connected");
-  }
+//  }
+//  else{
+    lcd.setCursor(1,0);
+    lcd.print("Running");
+//  }
 }
 
 void setup() {
-  Serial.begin(38400);
-  Blynk.begin(Serial, auth);
-  pinMode(Motor_R_Speed, OUTPUT);   // initializing the motors
-  pinMode(Motor_R_Dir, OUTPUT);
-  pinMode(Motor_L_Speed, OUTPUT);
-  pinMode(Motor_L_Dir, OUTPUT);
   lcd.init();                      // initializing the lcd 
   lcd.backlight();
   lcd.setCursor(1,0);
   lcd.print("Car Ready");
   lcd.setCursor(1,1);
-  lcd.print("Awaiting connection...");
+  lcd.print("connecting...");
+  pinMode(Motor_R_Speed, OUTPUT);   // initializing the motors
+  pinMode(Motor_R_Dir, OUTPUT);
+  pinMode(Motor_L_Speed, OUTPUT);
+  pinMode(Motor_L_Dir, OUTPUT);
+  Bluetooth.begin(9600);
 }
 
 void loop() {
-  Blynk.run();
-  if (Blynk.connected(){
-      LCD();
-      drive();
-  }
-  else{
-    LCD_status_updated = 0;
+  while(Bluetooth.available()>=2){
+    turn[0] = Bluetooth.read();
+    delay(10);
+    throttle[0] = Bluetooth.read();
+    lcd.clear();
     lcd.setCursor(1,0);
-    lcd.print("Disconnected");
+    lcd.print(turn[0]);
     lcd.setCursor(1,1);
-    lcd.print("Please reconnect...");
+    lcd.print(throttle[0]);
+    drive();
   }
 }
