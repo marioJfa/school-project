c2a19a33bf2d8c720458a649452eca420d9fb37c
marioJfa <mariojeries@gmail.com>
2021-04-01 22:50:40 +0300

    after adding the library and fixing all the bugs

--- school project/documents/Final/Final.ino
+++ school project/documents/Final/Final.ino
@@ -1,3 +1,17 @@
+//including essintial libraries
+
+  //For Motors
+  #include <MyMotorsLibrary.h>
+
+  //For LCD
+  #include <Wire.h> 
+  #include <LiquidCrystal_I2C.h>
+  #include <SoftwareSerial.h>
+
+//end
+
+
+
 //LCD
 //identifying LCD related variables
 #define LCD_Address 0x3F
@@ -9,30 +23,27 @@ int LCD_status_updated = 0;
 
 //identifying motor system related variables
 
-int Motor_R_Speed  = 5, Motor_R_Dir[] = {6,7}, Motor_L_Speed =9, Motor_L_Dir[] ={10,11};
+int Motor_R_Speed  = 5, Motor_R_Dir = 6, Motor_L_Speed =9, Motor_L_Dir = 10;
 int turn[2]; //[turn_value, turn_Dir]
+int turn_Dir;
 int turn_threshold = 220; //change to change the threshhold at which the car starts turning
+int turn_Speed = 200; //change to change the turning speed
 int turn_val; //used inside functions
-int turning_forward_direction = 0,turning_reverse_direction = 1; // reverse for reversing the turning directions
-int Max_turn_value = 45;
-int Min_turn_value = 10;
 int throttle[2]; //[throttle_value, throttle_Dir]
 bool throttle_Dir[2];
 int throttle_val[2];//used inside functions
-int throttle_forward_direction = 0,throttle_reverse_direction = 1; // reverse for reversing the throttling directions
 int Max_throttle_value = 255;
 int Min_throttle_value = 185; 
 
 // end
 
 
-//including essintial libraries
+//LCD inetializiation 
+LiquidCrystal_I2C lcd(LCD_Address,LCD_Columns,LCD_Rows);
+//end
 
-  //For LCD
-  #include <Wire.h> 
-  #include <LiquidCrystal_I2C.h>
-  #include <SoftwareSerial.h>
-
+// Motors inetialization
+MyMotorsLibrary Motors(Motor_R_Speed , Motor_R_Dir , Motor_L_Speed , Motor_L_Dir);
 //end
 
 
@@ -41,16 +52,14 @@ int Min_throttle_value = 185;
 SoftwareSerial Bluetooth(2,3);
 
 //end
-
-
 //turning functions
 
 void get_turn_val(int turn_input_val){
   if((turn_input_val%2)==1){
-    turn[1] = turning_forward_direction;
+    turn_Dir = Motors.turning_forward_direction;
   }
   else if((turn_input_val%2)==0){
-    turn[1] = turning_reverse_direction;
+    turn_Dir = Motors.turning_reverse_direction;
   }
 }
 
@@ -62,11 +71,11 @@ void get_throttle_value(int throttle_input_val){
   Serial.println("Throttle_Value");
   if((throttle_input_val%2)==1){
     throttle[0] = map(throttle_input_val,0,255,Min_throttle_value,Max_throttle_value);
-    throttle[1] = throttle_forward_direction;
+    throttle[1] = Motors.throttle_forward_direction;
   }
   else if((throttle_input_val%2)==0){
     throttle[0] = map(throttle_input_val,0,255,Min_throttle_value,Max_throttle_value);
-    throttle[1] = throttle_reverse_direction;
+    throttle[1] = Motors.throttle_reverse_direction;
   }
 }
 void get_throttle_Dir(){
@@ -79,41 +88,7 @@ void get_throttle_Dir(){
       throttle_Dir[1] =true;
     }
 }
-void Stop(){
-  Serial.println("stop");
-  analogWrite(Motor_R_Speed, 0);
-  digitalWrite(Motor_R_Dir, 0);
-  analogWrite(Motor_L_Speed, 0);
-  digitalWrite(Motor_L_Dir, 0);
-}
-//car turnning algorithms
-void Turn_Right(){
-  Serial.println("Turn_Right");
-  analogWrite(Motor_R_Speed, turn[0]);
-  digitalWrite(Motor_R_Dir[0], turning_reverse_direction);
-  digitalWrite(Motor_R_Dir[1], turning_forward_direction);
-  analogWrite(Motor_L_Speed, turn[0]);
-  digitalWrite(Motor_L_Dir[0], turning_forward_direction);
-  digitalWrite(Motor_L_Dir[1], turning_reverse_direction);
-}
-void Turn_Left(){
-  Serial.println("Turn_Left");
-  analogWrite(Motor_R_Speed, turn[0]);
-  digitalWrite(Motor_R_Dir[0], turning_reverse_direction);
-  digitalWrite(Motor_R_Dir[1], turning_forward_direction);
-  analogWrite(Motor_L_Speed, turn[0]);
-  digitalWrite(Motor_L_Dir[0], turning_forward_direction);
-  digitalWrite(Motor_L_Dir[1], turning_reverse_direction);
-}
-void Linear(){
-  Serial.println("Linear");
-  analogWrite(Motor_R_Speed, throttle[0]);
-  digitalWrite(Motor_R_Dir[0], throttle_Dir[1]);
-  digitalWrite(Motor_R_Dir[1], throttle_Dir[0]);
-  analogWrite(Motor_L_Speed, throttle[0]);
-  digitalWrite(Motor_L_Dir[0], throttle_Dir[1]);
-  digitalWrite(Motor_L_Dir[1], throttle_Dir[0]);
-}
+
 //end
 
 void drive(int throt,int turnn){
@@ -122,32 +97,28 @@ void drive(int throt,int turnn){
   get_throttle_value(throt);
   get_throttle_Dir();
   if(throttle[0] == 0){
-    Stop();
+    Motors.Stop();
   }
-  else if(turn[1] == 1){
+  else if(turn_Dir == 1){
     if(turn_val<=turn_threshold){
-      Linear();
+      Motors.Linear(throttle[0],throttle[1]);
     }
     else if(turn_val>turn_threshold){
-      Turn_Right();
+      Motors.Turn_Right(turn_Speed);
     }
   }
-  else if(turn[1] == 0){
+  else if(turn_Dir == 0){
     if(turn_val<=turn_threshold){
-      Linear();
+      Motors.Linear(throttle[0],throttle[1]);
     }
     else if(turn_val>turn_threshold){
-      Turn_Left();
+      Motors.Turn_Left(turn_Speed);
     }
   }
 }
 
 //end
 
-
-//LCD inetializiation 
-LiquidCrystal_I2C lcd(LCD_Address,LCD_Columns,LCD_Rows);
-//end
 
 void LCD(){
 //  if(LCD_status_updated == 0){
@@ -161,7 +132,6 @@ void LCD(){
 }
 
 void setup() {
-  turn[0]=200; //change to change the turning speed
   lcd.init();                      // initializing the lcd 
   lcd.backlight();
   lcd.setCursor(1,0);
@@ -187,7 +157,7 @@ void loop() {
       drive(throttle_val[0],turn_val);
     }
       else{
-      Stop();
+      Motors.Stop();
     }
 }
 }
--- school project/examples/librarytest/motorslibrary.cpp
+++ school project/examples/librarytest/motorslibrary.cpp
@@ -1,4 +1,10 @@
 #include "motorslibrary.h"
 
 Motor::Motor(int pin) {
+      void led_off(int pin){ //functions
+      digitalWrite(pin,LOW);
+    }
+    void led_on(int pin){
+      digitalWrite(pin,HIGH);
+    }
 };
